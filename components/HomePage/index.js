261196cdd013a9a2ddafc45b72953aca819dec51
Modi91 <alotaibimood@gmail.com>
2019-04-08 18:12:00 +0300

    addingComponentFolder

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Text, View } from "native-base";
 
-export default class HomePage extends Component {
+export default class index extends Component {
   render() {
     return (
       <View>
