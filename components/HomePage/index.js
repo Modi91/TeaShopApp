7010d91f0dc24b48f0d9a3fdf7de7f172aabf8ba
Modi91 <alotaibimood@gmail.com>
2019-04-08 18:00:39 +0300

    addingComponentFolder

---
+++ components/HomePage/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Text, View } from "native-base";
+
+export default class HomePage extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcom to Saudi Tea Shop</Text>
+      </View>
+    );
+  }
+}
